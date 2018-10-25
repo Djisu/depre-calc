59ab8d952cef2ca22d0a1ca5bf7ab89c688a0511
Paul Fleischer-Djoleto <pfleischer2002@yahoo.co.uk>
2018-10-25 01:37:03 +0100

    normal commit

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import jwt_decode from 'jwt-decode'
 import setAuthToken from './utils/setAuthToken'
@@ -77,8 +77,8 @@ class App extends Component {
           </div>
         </Router>
       </Provider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
